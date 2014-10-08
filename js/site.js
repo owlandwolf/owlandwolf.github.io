a8025e4ac2cef58061c503d8b882025a4e5d7285
svmatthews <matthews.sam@gmail.com>
2014-10-08 16:33:32 -0700

    lets work together

--- js/site.js
+++ js/site.js
@@ -1,3 +1,28 @@
-jQuery(document).ready(function($){
-  // mug
-});
+var lineNum = 0;
+function init() {
+  var lines = document.getElementsByClassName('underline');
+  flashLines(lines, lineNum);
+  lineTimer = setInterval(function(){
+    flashLines(lines, lineNum);
+  }, 2500);
+}
+
+function flashLines(l, n) {
+  console.log(l, n);
+  var prev = (n==0 ? 3 : n-1);
+  $('.underline').animate({
+    opacity: 0.5
+  }, 1000);
+  $('.underline:eq('+n+')').animate({
+    opacity: 1
+  }, 1000);
+  if(lineNum==l.length-1) {
+    lineNum = 0;
+  } else {
+    lineNum++;
+  }
+}
+
+// function 
+
+window.onload = init();
