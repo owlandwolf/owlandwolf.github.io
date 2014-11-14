4dc423d901ca4fccbff6fe8cd44c3fd79ca4ecde
svmatthews <matthews.sam@gmail.com>
2014-11-14 11:21:17 -0800

    map footer

--- js/site.js
+++ js/site.js
@@ -1,26 +1,28 @@
-var lineNum = 0;
 function init() {
-  var lines = document.getElementsByClassName('underline');
-  flashLines(lines, lineNum);
-  lineTimer = setInterval(function(){
-    flashLines(lines, lineNum);
-  }, 2500);
-}
 
-function flashLines(l, n) {
-  console.log(l, n);
-  var prev = (n==0 ? 3 : n-1);
-  $('.underline').animate({
-    opacity: 0.5
-  }, 1000);
-  $('.underline:eq('+n+')').animate({
-    opacity: 1
-  }, 1000);
-  if(lineNum==l.length-1) {
-    lineNum = 0;
-  } else {
-    lineNum++;
-  }
+  var map = L.map('contact').setView([47.599518, -122.25], 12);
+  L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png').addTo(map);
+    var popup = L.popup({
+    'offset': [395, 69]
+  }).setContent('<h3>Let\'s work together.</h3><p class="popup-info">314 1st Avenue South, Seattle, WA 98104<br><span>hello@theowlandwolf.com<br>206-854-5372</span></p>');
+
+  var marker = L.circleMarker([47.599518, -122.333970], {
+    'fillColor': '#000',
+    'stroke': false,
+    'fillOpacity': 1
+  }).setRadius(6).bindPopup(popup).addTo(map).openPopup();
+
+
+  $('a').on('click', function(){
+    if($(this).attr('href').indexOf('#')>-1) {
+      event.preventDefault();
+      var id = $(this).attr("href");
+      var top = $(id).offset().top;
+      $("html, body").animate({
+        scrollTop: top
+      }, 300); // you can set any time here
+    }
+  });
 }
 
 // function 
